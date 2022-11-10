9d0e1bf558fd9cede9d4557df956ad489042fd3b
Aqilalh <Aqilalhaidari@gmail.com>
2022-11-10 12:03:33 -0500

    doing work

---
+++ Dates.java
@@ -0,0 +1,18 @@
+public class Dates{
+
+public static void main(String[] args) {
+
+Date today = new Date();
+System.out.println("Today date is :" + today);
+
+LocalDate date = LocalDate.now();
+System.out.println(date);
+
+Test t = new Test();
+System.out.println(t);
+
+}
+
+
+
+}
